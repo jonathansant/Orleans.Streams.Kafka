d828e18618c29b045649d842c20deca4141223ee
Jonathan Sant <jonathan.sant@live.co.uk>
2018-11-21 16:52:14 +0100

    feat(producer): use `message.timeout.ms` instead of cancellation token

--- Orleans.Streams.Kafka/Producer/ProducerExtensions.cs
+++ Orleans.Streams.Kafka/Producer/ProducerExtensions.cs
@@ -1,28 +1,21 @@
-﻿using System;
-using System.Threading;
-using System.Threading.Tasks;
-using Confluent.Kafka;
+﻿using Confluent.Kafka;
 using Orleans.Streams.Kafka.Core;
+using System;
+using System.Threading.Tasks;
 
 namespace Orleans.Streams.Kafka.Producer
 {
 	public static class ProducerExtensions
 	{
-		public static async Task Produce(this Producer<byte[], KafkaBatchContainer> producer, KafkaBatchContainer batch, TimeSpan timeout)
-		{
-			using (var tokenSource = new CancellationTokenSource(timeout))
-			{
-				await producer.ProduceAsync(
-							batch.StreamNamespace,
-							new Message<byte[], KafkaBatchContainer>
-							{
-								Key = batch.StreamGuid.ToByteArray(),
-								Value = batch,
-								Timestamp = new Timestamp(DateTimeOffset.UtcNow)
-							}, 
-						tokenSource.Token
-					);
-			}
-		}
+		public static Task Produce(this Producer<byte[], KafkaBatchContainer> producer, KafkaBatchContainer batch)
+			=> producer.ProduceAsync(
+				batch.StreamNamespace,
+				new Message<byte[], KafkaBatchContainer>
+				{
+					Key = batch.StreamGuid.ToByteArray(),
+					Value = batch,
+					Timestamp = new Timestamp(DateTimeOffset.UtcNow)
+				}
+			);
 	}
 }
